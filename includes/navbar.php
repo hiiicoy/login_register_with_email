c4b4c79957878257c282214ca1515c970a53559c
mikqt4ever <supiezakim36@gmail.com>
2024-09-20 10:26:53 +0800

    first commit

---
+++ includes/navbar.php
@@ -0,0 +1,40 @@
+<div class="bg-success">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg bg-success">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">YICOY</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="index.php">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="dashboard.php">Dashboard</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="register.php">Sign up</a>
+                                </li>                              
+                                <li class="nav-item">
+                                    <a class="nav-link" href="login.php">Login</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+
+    .navbar-brand {
+        color: #fff;
+        font-size: bold;
+    }
+</style>
